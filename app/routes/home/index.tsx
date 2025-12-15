27d84b23abf4304bca6073acedb60cc9cf8971b8
Guanyu Lin <limkoaniun@gmail.com>
2025-12-15 22:11:15 +1100

    add responsive navbar

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -8,5 +8,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const now = new Date().toISOString();
+
+    if (typeof window === "undefined"){
+        console.log("Server Render At: ", now);
+    }else {
+        console.log("Client Hydration At: ", now);
+    }
+
     return <section>My App</section>;
 }
