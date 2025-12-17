108c77b0168c89f0777d29743df1b6a716676ec6
Guanyu Lin <limkoaniun@gmail.com>
2025-12-17 21:44:55 +1100

    add details page output

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,10 @@
-import {type RouteConfig, index, route, layout} from "@react-router/dev/routes";
+import {index, layout, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
     layout('./routes/layouts/home.tsx', [index("routes/home/index.tsx"),]),
     layout('./routes/layouts/main.tsx', [route("about", "./routes/about/index.tsx"),
         route("contact", "./routes/contact/index.tsx"),
         route("projects", "./routes/projects/index.tsx"),
+        route("projects/:id", "./routes/projects/details.tsx"),
         route("blog", "./routes/blog/index.tsx"),]),
 ] satisfies RouteConfig;
