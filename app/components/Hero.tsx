11cc671a29293615b8281fdfb9011529994fe7c4
Guanyu Lin <limkoaniun@gmail.com>
2025-12-17 22:49:06 +1100

    add pagination component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,14 @@
 import {Link} from "react-router";
+import React from "react";
 
-const Hero = ({name = "[NAME]", text = "..."}) => {
+type HeroProps = {
+   name?: string;
+   text?: string;
+};
+
+const Hero: React.FC<HeroProps> = (
+    {name = "[NAME]", text = "..."}
+) => {
     return (
         <header className="text-center py-20 px-4 bg-gray-900 text-white transition-colors duration-300">
             <h2 className="text-4xl font-bold mb-4">
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -2,6 +2,8 @@ import type {Route} from "./+types/index";
 import type {Project} from "~/types";
 import ProjectCard from "~/components/ProjectCard";
 import {useState} from "react";
+import Pagination from '~/components/Pagination';
+
 
 export async function loader({request}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
     const res = await fetch('http://localhost:8000/projects');
@@ -12,7 +14,7 @@ export async function loader({request}: Route.LoaderArgs): Promise<{ projects: P
 
 const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
     const {projects} = loaderData as { projects: Project[] };
-    const projectsPerPage = 5;
+    const projectsPerPage = 10;
 
     const [currentPage, setCurrentPage] = useState(1);
 
@@ -22,33 +24,17 @@ const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
     const indexOfFirst = indexOfLast - projectsPerPage;
     const currentProjects = projects.slice(indexOfFirst, indexOfLast);
 
-    const renderPagination = () => (
-        <div className="flex justify-center gap-2 mt-8">
-            {Array.from({ length: totalPages}, (_,idx) => (
-               <button
-                key={idx + 1}
-                className={`px-3 py-1 cursor-pointer rounded ${
-                   currentPage === idx + 1 ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'
-                }`}
-                onClick={() => setCurrentPage(idx + 1)}
-               >
-                   {idx + 1}
-               </button>
-                ))}
-        </div>
-    )
-
     return (
         <>
             <h2 className="text-3xl text-white font-bold mb-8">
                 Projects
             </h2>
             <div className="grid gap-6 sm:grid-cols-2">
-                {projects.map((project) => (
+                {currentProjects.map((project) => (
                    <ProjectCard project={project} key={project.id}/>
                 ))}
             </div>
-            { totalPages > 1 && renderPagination()}
+            { totalPages > 1 && <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={setCurrentPage} />}
         </>
     );
 };
