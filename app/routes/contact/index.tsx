335fb744ce12aeae6d555c97fb6690beb6e23de1
Guanyu Lin <limkoaniun@gmail.com>
2025-12-19 17:18:02 +1100

    add react router action to contact me form

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,11 +1,36 @@
-const ContactPage = () => {
+import type { Route } from "./+types";
+import {Form} from "react-router";
+
+export async function action({request}: Route.ActionArgs){
+   const formData = await request.formData();
+   const name = formData.get('name');
+    const email = formData.get('email');
+    const subject = formData.get('subject');
+    const message = formData.get('message');
+    const data = {
+        name,
+        email,
+        subject,
+        message
+    };
+
+    return {message: "Form submitted successfully", data};
+}
+
+const ContactPage = ({actionData}: Route.ComponentProps) => {
     return (
         <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
            <h2 className="text-3xl font-bold text-white mb-8 text-center">
                Contact me
            </h2>
 
-            <form className="space-y-6">
+            { actionData?.message ? (
+               <p className="mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">
+                   {actionData.message}
+               </p>
+            ) : null}
+
+            <Form method="post" className="space-y-6">
                <div>
                    <label htmlFor="name" className="block text-sm font-medium text-gray-300">
                        Full Name
@@ -34,7 +59,7 @@ const ContactPage = () => {
                 <button className="w-full text-white py-2 rounded-lg bg-blue-600 hover:bg-blue-700 cursor-pointer">
                     Send Message
                 </button>
-            </form>
+            </Form>
         </div>
     );
 };
