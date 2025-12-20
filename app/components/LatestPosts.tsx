cacab5b957e663e22a00459dfed66a81d34c0e18
Guanyu Lin <limkoaniun@gmail.com>
2025-12-20 16:37:49 +1100

    add latest post component

---
+++ app/components/LatestPosts.tsx
@@ -0,0 +1,47 @@
+import type {PostMeta} from "~/types";
+import {Link} from "react-router";
+
+type LastestPostsProps = {
+    posts: PostMeta[];
+    limit?: number
+}
+
+const LatestPosts = ({posts, limit = 3}: LastestPostsProps) => {
+    const sortedPosts = [...posts].sort((a: PostMeta, b: PostMeta) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+    })
+
+    const latest = sortedPosts.slice(0, limit);
+
+    return (
+        <div>
+            <section className="max-w-6xl mx-auto px-6 py-12">
+                <h2 className="text-2xl font-bold mb-6 text-white">
+                    Lastest Posts
+                </h2>
+                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    {latest.map((post) => (
+                        <Link
+                            key={post.slug}
+                            to={`/blog/${post.slug}`}
+                            className="block p-4 border-gray-700 rounded-lg bg-gray-800 hover:shadow-md transition"
+                        >
+                            <h3 className="text-lg font-semibold text-blue-400 mb-1">
+                                {post.title}
+                            </h3>
+
+                            <p className="text-sm text-gray-300">
+                                {post.excerpt}
+                            </p>
+                            <span className="block mt-3 text-xs text-gray-400">
+                                {new Date(post.date).toLocaleDateString()}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default LatestPosts;
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,7 +1,8 @@
 import type {Route} from "./+types/index";
 import FeaturedProjects from "~/components/FeaturedProjects";
-import type {Project} from "~/types";
+import type {PostMeta, Project} from "~/types";
 import AboutPreview from "~/components/AboutPreview";
+import LatestPosts from "~/components/LatestPosts";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -10,19 +11,33 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-export async function loader({request}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-    const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-    const data = await res.json();
+export async function loader({request}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+    const url = new URL(request.url);
 
-    return {projects: data};
+    const [projectRes, postRes] = await Promise.all([
+        fetch(`${import.meta.env.VITE_API_URL}/projects`),
+        fetch(new URL('/posts-meta.json', url))
+    ]);
+
+    if(!projectRes.ok || !postRes.ok){
+        throw new Error("Failed to fetch projects or posts");
+    }
+
+    const [projects, posts] = await Promise.all([
+        projectRes.json(),
+        postRes.json()
+    ]);
+
+    return {projects, posts};
 }
 
 const HomePage = ({loaderData}: Route.ComponentProps) => {
-    const {projects} = loaderData;
+    const {projects, posts} = loaderData;
 
     return <>
         <FeaturedProjects projects={projects} count={4}/>
         <AboutPreview/>
+        <LatestPosts posts={posts} limit={5}/>
     </>;
 }
 
