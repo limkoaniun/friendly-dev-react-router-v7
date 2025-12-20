a0da753c7aa635e3076ddeaa95a3bf95841af0f3
Guanyu Lin <limkoaniun@gmail.com>
2025-12-20 15:32:08 +1100

    add blog post filter

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -3,42 +3,67 @@ import type {PostMeta} from "~/types";
 import PostCard from "~/components/PostCard";
 import {useState} from "react";
 import Pagination from "~/components/Pagination";
+import PostFilter from "~/components/PostFilter";
 
-export async function loader({request}: Route.LoaderArgs):Promise<{posts: PostMeta[]}>{
+export async function loader({request}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
     const url = new URL('/posts-meta.json', request.url);
     const res = await fetch(url.href);
 
-    if(!res.ok) throw new Error("Failed to fetch data");
+    if (!res.ok) throw new Error("Failed to fetch data");
 
     const data = await res.json();
 
     data.sort((a: PostMeta, b: PostMeta) => {
         return new Date(b.date).getTime() - new Date(a.date).getTime();
     });
-    
+
     return {posts: data};
 }
 
 const BlogPage = ({loaderData}: Route.ComponentProps) => {
+    const [searchQuery, setSearchQuery] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const postsPerPage = 3;
+
     const {posts} = loaderData;
 
-    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const filteredPosts = posts.filter((post) => {
+        const query = searchQuery.toLowerCase();
+        return (
+            post.title.toLowerCase().includes(query) || post.excerpt.toLowerCase().includes(query)
+        )
+    });
+
+    const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
     const indexOfLast = currentPage * postsPerPage;
     const indexOfFirst = indexOfLast - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+    const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
 
     return (
         <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
-           <h2 className="text-3xl text-white font-bold mb-8">
-              Blog
-           </h2>
-            {currentPosts.map((post) =>
-                (
-                    <PostCard post={post} key={post.slug}/>
-                )
-            )}
+            <h2 className="text-3xl text-white font-bold mb-8">
+                Blog
+            </h2>
+
+            <PostFilter searchQuery={searchQuery} onSearchChange={(query) => {
+                setSearchQuery(query);
+                setCurrentPage(1);
+            }}/>
+
+            <div className="space-y-8">
+                {currentPosts.length === 0
+                    ? (<p className="text-gray-400 text-center">
+                        No Posts Found
+                    </p>)
+                    : (
+                        currentPosts.map((post) =>
+                            (
+                                <PostCard post={post} key={post.slug}/>
+                            )
+                        )
+                    )}
+            </div>
+
 
             {
                 totalPages > 1 && (
